5ad42666de4ad38a50f19bfbad384999c72292b9
victorr0909 <namit09092001@gmail.com>
2024-10-05 17:01:00 +0530

    Node.js revision

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Hi        
+const fs = require("fs")
+
+fs.writeFile('msg.txt',"Hello from Node",(err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+if (err) throw err;
+console.log(data);
+});
