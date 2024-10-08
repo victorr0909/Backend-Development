21cbc87336301954c3f9049c7152d29036af3ec3
victorr0909 <namit09092001@gmail.com>
2024-10-08 21:49:06 +0530

    Express JS Continued

---
+++ 3.4 Middleware/index1.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser"
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({expected:true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body)
+});
